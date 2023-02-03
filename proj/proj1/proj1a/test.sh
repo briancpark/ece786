81147a3d485322b303d747a325c97a7fbecbd995
Brian Park <bcpark@ncsu.edu>
2023-02-03 00:06:49 -0500

    remove comment

--- proj/proj1/proj1a/test.sh
+++ proj/proj1/proj1a/test.sh
@@ -5,17 +5,6 @@ if [ ! -f quamsim ]; then
     exit 1
 fi
 
-
-# Iterate through all of the input files and execute the program
-# Then compare the outputs to the ones in output directory
-# Here's an example use case
-# quamsim input/input_qc4_q0.txt >> input_qc4_q0.out
-# diff input_qc4_q0.out output/output_qc4_q0.out
-# Note that the formats of inputs are in input/input*.txt and outputs are in output/output*.txt
-
-# Iterate through all of the input files and execute the program
-# Then compare the outputs to the ones in output directory
-
 for file in input/input*.txt
 do
     echo "Testing $file"
