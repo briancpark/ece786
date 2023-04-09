191d34d67b332ffda4e6b743e0e39bf40636357c
Brian Park <bcpark@ncsu.edu>
2023-04-08 23:07:43 -0400

    update progress... not close

--- proj/proj3/proj3b/quamsimV2.cu
+++ proj/proj3/proj3b/quamsimV2.cu
@@ -7,30 +7,52 @@
 #include <vector>
 using namespace std;
 
-__device__ void inline quantum_kernel(float* U, float* a, float* output, size_t qid, size_t N,
-                                      int tid) {
-    if (tid & qid)
-        output[tid] = U[3] * a[tid] + U[2] * a[tid - qid];
-    else
-        output[tid] = U[1] * a[tid + qid] + U[0] * a[tid];
-    __syncthreads();
-    __syncwarp();
-}
+#define FRAGMENT_SIZE     (1 << 6) // 2^6
+#define THREADS_PER_BLOCK (1 << 5) // 2^5
 
 __global__ void quantum_simulation_gpu(float* U_0, float* U_1, float* U_2, float* U_3, float* U_4,
                                        float* U_5, float* a, float* output, size_t qubit0,
                                        size_t qubit1, size_t qubit2, size_t qubit3, size_t qubit4,
-                                       size_t qubit5, size_t N, size_t* auxillary_array) {
+                                       size_t qubit5, size_t N, size_t* auxillary_array,
+                                       size_t* offsets) {
 
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    int num_fragments = N / FRAGMENT_SIZE;
 
-    // if (tid >= N) {
-    //     return;
-    // }
+    if (tid >= N / 2) {
+        return;
+    }
 
-    __shared__ float a_shared[64];
-    __shared__ float output_shared[64];
+    __shared__ float a_shared[FRAGMENT_SIZE];
+    __shared__ float output_shared[FRAGMENT_SIZE];
 
+    printf("num_fragments: %d\n", num_fragments);
+
+    // Load the fragment from global memory to shared memory
+
+    // todo; eneumeratie
+    size_t offset = 0;
+    if (blockIdx.x) {
+        offset = 1 << offsets[blockIdx.x - 1];
+    }
+    printf("blockIdx.x: %d\n", blockIdx.x);
+    printf("offset: %d\n", offset);
+
+    for (int i = threadIdx.x * 2; i < FRAGMENT_SIZE; i += blockDim.x * 2) {
+        a_shared[i] = a[i + offset];
+        a_shared[i + 1] = a[i + 1 + offset];
+    }
+    __syncthreads();
+    __syncwarp();
+    if (tid % 32 == 0) {
+        for (int i = 0; i < FRAGMENT_SIZE; i++) {
+            printf("a_shared[%d] = %f\n", i, a_shared[i]);
+        }
+    }
+    __syncthreads();
+    __syncwarp();
+
+    __syncthreads();
     size_t qid0 = 1 << qubit0;
     size_t qid1 = 1 << qubit1;
     size_t qid2 = 1 << qubit2;
@@ -38,177 +60,39 @@ __global__ void quantum_simulation_gpu(float* U_0, float* U_1, float* U_2, float
     size_t qid4 = 1 << qubit4;
     size_t qid5 = 1 << qubit5;
 
-    int tid1 = tid;
-    // int tid0 = tid1 + 1;
-    quantum_kernel(U_0, a, output, qid0, N, tid1);
-    __syncthreads();
-    __syncwarp();
-
-    // copy output to a
-    a[tid1] = output[tid1];
-    // a[tid0] = output[tid0];
-    __syncthreads();
-    __syncwarp();
-    quantum_kernel(U_1, a, output, qid1, N, tid1);
+    // example: 0 2 3 4 5 6
+    // binary  0 4 8 16 32 64
 
     __syncthreads();
     __syncwarp();
-
-    // copy output to a
-    a[tid1] = output[tid1];
-    // a[tid0] = output[tid0];
-    __syncthreads();
-    __syncwarp();
-    quantum_kernel(U_2, a, output, qid2, N, tid1);
+    float* Us[6] = {U_0, U_1, U_2, U_3, U_4, U_5};
+    size_t qids[6] = {qid0, qid1, qid2, qid3, qid4, qid5};
+    int i = threadIdx.x * 2;
 
     __syncthreads();
     __syncwarp();
+    for (int gate = 0; gate < 6; gate++) {
+        float* U = Us[gate];
 
-    // copy output to a
-    a[tid1] = output[tid1];
-    // a[tid0] = output[tid0];
-    __syncthreads();
-    __syncwarp();
-    quantum_kernel(U_3, a, output, qid3, N, tid1);
+        float x0 = a_shared[i];
+        float x1 = a_shared[i + 1];
+        __syncthreads();
+        __syncwarp();
+        a_shared[i] = U[0] * x0 + U[2] * x1;
+        a_shared[i + 1] = U[1] * x0 + U[3] * x1;
 
-    __syncthreads();
-    __syncwarp();
-
-    // copy output to a
-    a[tid1] = output[tid1];
-    // a[tid0] = output[tid0];
-    __syncthreads();
-    __syncwarp();
-    quantum_kernel(U_4, a, output, qid4, N, tid1);
+        __syncthreads();
+        __syncwarp();
+    }
 
     __syncthreads();
     __syncwarp();
-
-    // copy output to a
-    a[tid1] = output[tid1];
-    // a[tid0] = output[tid0];
-    __syncthreads();
-    __syncwarp();
-    quantum_kernel(U_5, a, output, qid5, N, tid1);
+    // Store the fragment from shared memory to global memory
+    output[tid * 2 + offset] = a_shared[i];
+    output[(tid * 2) + 1 + offset] = a_shared[i + 1];
 
     __syncthreads();
     __syncwarp();
-
-    // if (tid1 == 0) {
-    //     // printf(
-    //     // "qubit0 = %lu, qubit1 = %lu, qubit2 = %lu, qubit3 = %lu, qubit4 = %lu, qubit5 =
-    //     %lu\n",
-    //     // qubit0, qubit1, qubit2, qubit3, qubit4, qubit5);
-    //     printf("qid0 = %lu, qid1 = %lu, qid2 = %lu, qid3 = %lu, qid4 = %lu, qid5 = %lu\n", qid0,
-    //            qid1, qid2, qid3, qid4, qid5);
-    // }
-    // __shared__ float a_shared[64];
-    // __shared__ float output_shared[64];
-
-    // size_t t = N - 1;
-    // size_t bid = blockDim.x * blockIdx.x;
-    // if (tid1 == 0) {
-    //     printf("t = %lu\n", t);
-    // }
-    // // set t's 1s to 0s based on qids
-    // size_t bitmask = qid0 | qid1 | qid2 | qid3 | qid4 | qid5;
-    // t &= ~bitmask;
-
-    // if (tid1 == 0) {
-    //     printf("bitmask = %lu\n", bitmask);
-    //     printf("t = %lu\n", t);
-    // }
-
-    // printf("bid = %lu\n", bid);
-
-    // // based on the bid, set the t bits to 1s
-    // // THIS IS ONLY FOR 128
-    // if (bid < 32) {
-    //     t = 0;
-    // } else {
-    //     t = 2;
-    // }
-    // __syncthreads();
-    // __syncwarp();
-
-    // if (tid1 % 32 == 0) {
-    //     for (int i = 0; i < 64; i++) {
-    //         printf("auxillary_array[%d] = %lu\n", i, auxillary_array[i]);
-    //         a_shared[i] = a[auxillary_array[i] + t];
-    //     }
-    // }
-
-    // __syncthreads();
-    // __syncwarp();
-
-    // if (tid1 == 0) {
-    //     for (int i = 0; i < 64; i++) {
-    //         printf("a_shared[%d] = %f\n", i, a_shared[i]);
-    //     }
-    // }
-    // __syncthreads();
-    // __syncwarp();
-
-    // // Apply the six single-qubit gates on each of the fragments and get the results. Similar to
-    // // applying 6 gates on a 6-qubit circuit, you can split this process into 6 steps: applying
-    // // the first gate to qubit 0, applying the second gate to qubit 1, applying the third gate
-    // // to qubit 2, applying the fourth gate to qubit 3, applying the fifth gate to qubit 4, and
-    // // applying the sixth gate to qubit 5. And for each of the steps, the computation is the
-    // // same as in PA1, and here since we have 2^6 values that need to be calculated in each
-    // // thread block, there will be 2^5 threads in each thread block, one thread will do one
-    // // matrix multiplication
-
-    // // The auxiliary array is used to store the indices of the 2^6 values that need to be
-    // // calculated in each thread block.
-    // // We can perform quantum simulation by using the auxiliary array to map the 2^6 values where
-    // // they are stored consecutively
-
-    // // Apply the first gate to qubit 0
-    // __syncthreads();
-    // __syncwarp();
-    // size_t tid_0 = threadIdx.x * 2;
-    // size_t tid_1 = threadIdx.x * 2 + 1;
-
-    // if (qid0&)
-
-    //     __syncthreads();
-    // __syncwarp();
-
-    // // copy output_shared back to a_shared
-    // a_shared[tid_0] = output_shared[tid_0];
-    // a_shared[tid_1] = output_shared[tid_1];
-
-    // __syncthreads();
-    // __syncwarp();
-
-    // // if (tid1 % 32 == 0) {
-    // //     for (int i = 0; i < 64; i++) {
-    // //         printf("a_shared[%d] = %f\n", i, a_shared[i]);
-    // //         printf("output_shared[%d] = %f\n", i, output_shared[i]);
-    // //     }
-    // // }
-
-    // // printf("%f * %f = %f\n", U_1[0], a_shared[tid_0], U_1[0] * a_shared[tid_0]);
-    // // printf("%f * %f = %f\n", U_1[1], a_shared[tid_1], U_1[1] * a_shared[tid_1]);
-    // // printf("%f * %f = %f\n", U_1[2], a_shared[tid_0], U_1[2] * a_shared[tid_0]);
-    // // printf("%f * %f = %f\n", U_1[3], a_shared[tid_1], U_1[3] * a_shared[tid_1]);
-
-    // if (tid0 & qid0) {
-    //     output_shared[tid_0] = U_1[0] * a_shared[tid_0] + U_1[1] * a_shared[tid_1];
-    // }
-    // output_shared[tid_0] = U_1[0] * a_shared[tid_0] + U_1[1] * a_shared[tid_1];
-    // output_shared[tid_1] = U_1[2] * a_shared[tid_0] + U_1[3] * a_shared[tid_1];
-
-    // // output_shared[tid_0] = U_2[0] * a_shared[tid_0] + U_2[1] * a_shared[tid_1];
-    // // output_shared[tid_1] = U_2[2] * a_shared[tid_0] + U_2[3] * a_shared[tid_1];
-
-    // __syncthreads();
-    // __syncwarp();
-    // // copy back output_shared to output
-    // output[auxillary_array[tid_0] + t] = output_shared[tid_0];
-    // output[auxillary_array[tid_1] + t] = output_shared[tid_1];
-    // __syncthreads();
-    // __syncwarp();
     return;
 }
 
@@ -242,11 +126,6 @@ int main(int argc, char** argv) {
         for (int i = 0; i < 4; i++) {
             input_file >> U[i];
         }
-        // TODO (bcp) DEBUG
-        // for (int i = 0; i < 4; i++) {
-        //     cout << U[i] << " ";
-        // }
-        // cout << endl;
     }
 
     // Read in the vector until we hit an empty line
@@ -258,11 +137,6 @@ int main(int argc, char** argv) {
         a.push_back(stof(line));
     }
 
-    // Print the vector contents:
-    // for (int i = 0; i < a.size(); i++) {
-    //     printf("%.3f\n", a[i]);
-    // }
-
     // Read in the qubit
     input_file >> qubit_0;
     input_file >> qubit_1;
@@ -270,14 +144,6 @@ int main(int argc, char** argv) {
     input_file >> qubit_3;
     input_file >> qubit_4;
     input_file >> qubit_5;
-
-    // TODO (bcp) DEBUG: Print the qubits:
-    // cout << qubit_0 << endl;
-    // cout << qubit_1 << endl;
-    // cout << qubit_2 << endl;
-    // cout << qubit_3 << endl;
-    // cout << qubit_4 << endl;
-    // cout << qubit_5 << endl;
 
     float* output = (float*)malloc(a.size() * sizeof(float));
 
@@ -306,22 +172,6 @@ int main(int argc, char** argv) {
 
     int threadsPerBlock = 32;
     int blocksPerGrid = (a.size() + threadsPerBlock - 1) / threadsPerBlock;
-    // cout << "Blocks per grid: " << blocksPerGrid << endl;
-    // cout << "Threads per block: " << threadsPerBlock << endl;
-
-    /*
-    (const float* U_0, const float* U_1, const float* U_2,
-    const float* U_3, const float* U_4, const float* U_5,
-    const float* a, float* output, size_t qubit0, size_t
-    qubit1, size_t qubit2, size_t qubit3, size_t qubit4, size_t qubit5, size_t N) {
-    */
-
-    // cout << qubit_0 << endl;
-    // cout << qubit_1 << endl;
-    // cout << qubit_2 << endl;
-    // cout << qubit_3 << endl;
-    // cout << qubit_4 << endl;
-    // cout << qubit_5 << endl;
 
     size_t* auxillary_array = (size_t*)malloc(64 * sizeof(size_t));
     size_t* auxillary_array_gpu;
@@ -345,14 +195,48 @@ int main(int argc, char** argv) {
     cudaMemcpy(auxillary_array_gpu, auxillary_array, 64 * sizeof(size_t), cudaMemcpyHostToDevice);
 
     // print the contents of the auxillary array in binary
-    // for (int i = 0; i < 64; i++) {
-    //     // binary
-    //     cout << bitset<6>(auxillary_array[i]) << endl;
-    // }
+    for (int i = 0; i < 64; i++) {
+        // binary
+        cout << bitset<6>(auxillary_array[i]) << endl;
+    }
+
+    // length of offset is determined by log2(a.size()) - 6
+    size_t offsets_len = (size_t)log2(a.size()) - 6;
+    size_t* offsets = (size_t*)malloc(offsets_len * sizeof(size_t));
+    size_t* offsets_gpu;
+    cudaMalloc(&offsets_gpu, offsets_len * sizeof(size_t));
+    cout << "offsets_len: " << offsets_len << endl;
+
+    size_t bitmask = 0;
+    // set the bitmask to 1 at the qubit indices
+    for (int i = 0; i < 6; i++) {
+        bitmask |= (1 << qubits[i]);
+        cout << "bitmask: " << bitset<16>(bitmask) << endl;
+    }
+
+    // Determine the bits that are set to 0 in the bitmask and store them in the offsets array
+    int bitid = 0;
+    cout << "log(a.size()): " << log(a.size()) << endl;
+    for (int i = 0; i < log2(a.size()); i++) {
+        cout << "bitmask: " << bitset<16>(bitmask) << endl;
+        cout << "i: " << i << endl;
+        cout << "bitmask | (1 << i): " << bitset<16>(bitmask | (1 << i)) << endl;
+        if ((bitmask | (1 << i)) != bitmask) {
+            offsets[bitid] = i;
+            bitid++;
+        }
+    }
+
+    // print the contents of the offsets array
+    for (int i = 0; i < offsets_len; i++) {
+        cout << "offsets[" << i << "]: " << offsets[i] << endl;
+    }
+
+    cudaMemcpy(offsets_gpu, offsets, offsets_len * sizeof(size_t), cudaMemcpyHostToDevice);
 
     quantum_simulation_gpu<<<blocksPerGrid, threadsPerBlock>>>(
         U_0_gpu, U_1_gpu, U_2_gpu, U_3_gpu, U_4_gpu, U_5_gpu, a_gpu, output_gpu, qubit_0, qubit_1,
-        qubit_2, qubit_3, qubit_4, qubit_5, a.size(), auxillary_array_gpu);
+        qubit_2, qubit_3, qubit_4, qubit_5, a.size(), auxillary_array_gpu, offsets_gpu);
 
     // err
     cudaError_t err;
@@ -373,6 +257,7 @@ int main(int argc, char** argv) {
         printf("%.3f\n", output[i]);
     }
 
+    cudaFree(offsets_gpu);
     cudaFree(U_0_gpu);
     cudaFree(U_1_gpu);
     cudaFree(U_2_gpu);
