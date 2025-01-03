1652c813e3ff59aa72afe6593f7a5f67f759616d
Brian Park <bcpark@ncsu.edu>
2025-01-03 22:45:26 +0900

    Fix Makefile (#18)

--- proj/proj1/proj1a/test.sh
+++ proj/proj1/proj1a/test.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 make clean
 make -j
 
-for file in input/input*.txt
-do
-    echo "Testing $file"
-    ./quamsimV1 $file >> $file.out
-    output_file=${file//input/output}
-    diff $file.out $output_file
-    rm $file.out
-done
+# Function to test and check for differences
+test_and_diff() {
+    local executable=$1
+    for file in input/input*.txt; do
+        echo "Testing $file with $executable"
+        ./$executable $file >> "$file.out"
+        output_file=${file//input/output}
+        if ! diff "$file.out" "$output_file"; then
+            echo "Error: Output for $file with $executable differs from expected output."
+            exit 1
+        fi
+        rm "$file.out"
+    done
+}
 
-for file in input/input*.txt
-do
-    echo "Testing $file"
-    ./quamsimV2 $file >> $file.out
-    output_file=${file//input/output}
-    diff $file.out $output_file
-    rm $file.out
-done
+# Test both executables
+test_and_diff "quamsimV1"
+test_and_diff "quamsimV2"
+
